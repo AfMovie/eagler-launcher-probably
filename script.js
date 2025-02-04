1899b8c195f7d212800d4cfc9866b0774f56a6f9
cobblesteve-dev <channelediting521@gmail.com>
2025-02-04 17:33:56 -0600

    Figure out music settings tomorrow

--- script.js
+++ script.js
@@ -47,6 +47,8 @@ document.addEventListener('keydown', (event) => {
     }
 });
 
+const audio = new Audio(`${Math.floor(Math.random() * 4 + 1)}.mp3`);
+
 document.addEventListener('click', (event) => {
     if (event.target === closeButton) {
         close();
@@ -74,8 +76,6 @@ document.addEventListener('click', (event) => {
     }
 });
 
-window.onload = music;
-
 const audioFiles = [
     "1.mp3",
     "2.mp3",
@@ -83,22 +83,81 @@ const audioFiles = [
     "4.mp3"
 ];
 
-const audio = new Audio();
+window.onload = launch;
 
 function music() {
-    const randomIndex = Math.floor(Math.random() * audioFiles.length);
-    audio.src = audioFiles[randomIndex];
-    audio.play();
-} 
+    window.addEventListener("DOMContentLoaded", () => {
+        
+        const playAudio = () => {
+            audio.play().catch(error => console.log("Autoplay blocked: ", error));
+            document.getElementById("launchbg").removeEventListener("mouseenter", playAudio);
+        };
+        
+        document.getElementById("launchbg").addEventListener("mouseenter", playAudio);
+
+        const musicToggle = document.getElementById('musicToggle');
+    });
+}
+
+function toggleMusic() {
+    const toggle = document.getElementById('musicToggle');
+
+    if (toggle.unchecked) {
+        audio.pause();
+    } else {
+        audio.play();
+    }
+}
+
+function launch() {
+    const logo1 = document.getElementById('logo1');
+    const logo2 = document.getElementById('logo2');
+    const logo3 = document.getElementById('logo3');
+    const background = document.getElementById('launchbg');
+
+    setTimeout(() => {
+        logo1.style.opacity = '1';
+        logo1.style.width = '1250px';
+    }, 500);
+
+    setTimeout(() => {
+        logo2.style.opacity = '1';
+        logo2.style.scale = '1.1';
+    }, 4000);
+
+    setTimeout(() => {
+        logo3.style.opacity = '1';
+        logo3.style.scale = '1.1';
+    }, 7000);
+
+    setTimeout(() => {
+        logo1.style.opacity = '0';
+    }, 3000);
+
+    setTimeout(() => {
+        logo2.style.opacity = '0';
+    }, 6000);
+
+    setTimeout(() => {
+        logo3.style.opacity = '0';
+    }, 9000);
+
+    setTimeout(() => {
+        background.style.opacity = '0';
+    }, 10000);
+
+    setTimeout(() => {
+        background.style.display = 'none';
+        logo1.style.display = 'none';
+        logo2.style.display = 'none';
+        logo3.style.display = 'none';
+    }, 11000);
+}
 
 audio.addEventListener("ended", music);
 
 window.addEventListener('change', event => {
-    const musicToggle = document.getElementById('musicToggle');
 
-    if (musicToggle.checked === true) {
-        music();
-    } else {
-        audio.pause();
-    }
-})
+});
+
+music()
